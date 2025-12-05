7715aa6fb5c35852736ecea71b8179da59f1d843
yeutushenkax <yeutushenkax@gmail.com>
2025-12-05 21:59:09 +0000

    a

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -68,3 +68,13 @@ def test_get_nth_fibonacci_ten():
 
    # Assert
    assert result == 55
+def test_get_nth_fibonacci_ten():
+   """Test with n=10."""
+   # Arrange
+   n = 10
+
+   # Act
+   result = get_nth_fibonacci(n)
+
+   # Assert
+   assert result == 55
